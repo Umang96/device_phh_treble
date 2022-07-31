9ba1d7b168e3bc3c9e195f7a634d9527ea4aedfe
Umang96 <umangleekha3@gmail.com>
2022-07-31 10:15:11 +0530

    Revert "Include lawnchair"
    This reverts commit eb35847c339bec4c2eb614400eacd841f80daa43.

--- base.mk
+++ base.mk
@@ -54,7 +54,6 @@ TARGET_USES_ION := true
 TARGET_SCREEN_DENSITY := 420
 
 $(call inherit-product, vendor/hardware_overlay/overlay.mk)
-$(call inherit-product, vendor/aosp/prebuilt/Lawnchair/lawnchair.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
