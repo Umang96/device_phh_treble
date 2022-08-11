aae25e36319addaf8bf3ddacb2de349ac30e0a94
Umang96 <umangleekha3@gmail.com>
2022-08-11 21:19:03 +0530

    Minor adjustments to 6gb dalvik props

--- set-dalvik-props.sh
+++ set-dalvik-props.sh
@@ -18,10 +18,10 @@ elif [ $MemTotal -gt 7000000 ]; then
     setprop dalvik.vm.heapminfree "8m"
     setprop dalvik.vm.heapmaxfree "48m"
 else
-    setprop dalvik.vm.heapstartsize "16m"
-    setprop dalvik.vm.heapgrowthlimit "256m"
+    setprop dalvik.vm.heapstartsize "12m"
+    setprop dalvik.vm.heapgrowthlimit "192m"
     setprop dalvik.vm.heapsize "512m"
     setprop dalvik.vm.heaptargetutilization "0.5"
     setprop dalvik.vm.heapminfree "8m"
-    setprop dalvik.vm.heapmaxfree "32m"
+    setprop dalvik.vm.heapmaxfree "24m"
 fi
