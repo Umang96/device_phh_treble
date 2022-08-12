e7b21504e7808e732d4eba383c71cbcd2e11298f
Umang96 <umangleekha3@gmail.com>
2022-08-12 19:00:17 +0530

    Update swappiness, dalvik, special app permissions

--- set-dalvik-props.sh
+++ set-dalvik-props.sh
@@ -4,22 +4,22 @@ setprop sys.phh.test.prop "7m"
 MemTotalStr=`cat /proc/meminfo | grep MemTotal`
 MemTotal=${MemTotalStr:16:8}
 if [ $MemTotal -gt 9000000 ]; then
-    setprop dalvik.vm.heapstartsize "24m"
-    setprop dalvik.vm.heapgrowthlimit "384m"
-    setprop dalvik.vm.heapsize "512m"
-    setprop dalvik.vm.heaptargetutilization "0.42"
-    setprop dalvik.vm.heapminfree "8m"
-    setprop dalvik.vm.heapmaxfree "56m"
-elif [ $MemTotal -gt 7000000 ]; then
-    setprop dalvik.vm.heapstartsize "24m"
+    setprop dalvik.vm.heapstartsize "20m"
     setprop dalvik.vm.heapgrowthlimit "256m"
     setprop dalvik.vm.heapsize "512m"
     setprop dalvik.vm.heaptargetutilization "0.46"
     setprop dalvik.vm.heapminfree "8m"
     setprop dalvik.vm.heapmaxfree "48m"
+elif [ $MemTotal -gt 7000000 ]; then
+    setprop dalvik.vm.heapstartsize "16m"
+    setprop dalvik.vm.heapgrowthlimit "256m"
+    setprop dalvik.vm.heapsize "512m"
+    setprop dalvik.vm.heaptargetutilization "0.50"
+    setprop dalvik.vm.heapminfree "8m"
+    setprop dalvik.vm.heapmaxfree "32m"
 else
     setprop dalvik.vm.heapstartsize "12m"
-    setprop dalvik.vm.heapgrowthlimit "192m"
+    setprop dalvik.vm.heapgrowthlimit "256m"
     setprop dalvik.vm.heapsize "512m"
     setprop dalvik.vm.heaptargetutilization "0.5"
     setprop dalvik.vm.heapminfree "8m"
